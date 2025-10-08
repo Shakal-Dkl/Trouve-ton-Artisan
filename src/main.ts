2afc71fa0027ece0bc2909df305515f296cd995f
Shakal-Dkl <160112562+Shakal-Dkl@users.noreply.github.com>
2025-10-08 10:13:28 +0200

    gj

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { App } from './app/app';
 import { routes } from './app/app.routes';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(App, {
+bootstrapApplication(App, appConfig).catch(err => console.error(err));
   providers: [
     provideRouter(routes)
   ]
-}).catch(err => console.error(err));
